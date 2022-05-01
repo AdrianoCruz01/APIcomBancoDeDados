75a8df6b611bc8b28810d4974547a626f3324ec5
AdrianoCruz01 <adrianocruz.reis@gmail.com>
2022-05-01 18:48:05 -0300

    Banco de Dados incluso

---
+++ bancoDeDados/banco.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Hackthon;
+
+CREATE TABLE produtos (
+	id serial,
+  	nome varchar(30) NOT NULL,
+  	empresa varchar(30) NOT NULL,
+  	foto varchar(800),
+  	valor int NOT NULL,
+  	descricao text NOT NULL,
+  	especificacoes varchar(50) NOT NULL,
+  	etiquetasEcologicas varchar(100) NULL
+);
