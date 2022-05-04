03ccc2d3bbfa87c8873f9a3ed1cbdbee6a18a125
AdrianoCruz01 <adrianocruz.reis@gmail.com>
2022-05-04 05:27:26 -0300

    Inplementação das funções alterar produto e excluir produto

--- controladores/produto.js
+++ controladores/produto.js
@@ -3,9 +3,8 @@ const conexao = require('../conexao');
 const criarProduto = async (req, res) => {
     const { nome, empresa, foto, valor, descricao, especificacoes, etiquetasEcologicas } = req.body;
 
-
     if (!nome, !empresa, !valor, !descricao, !especificacoes) {
-        return res.status(400).json('Campo obrigatório não preenchido.');
+        return res.status(400).json('Campo obrigatório não preenchido!');
     }
 
     try {
@@ -15,10 +14,10 @@ const criarProduto = async (req, res) => {
         const umProduto = await conexao.query(query, [nome, empresa, foto, valor, descricao, especificacoes, etiquetasEcologicas]);
 
         if (umProduto.rowCount === 0) {
-            return res.status(400).json('Não foi possível cadastrar o produto.');
+            return res.status(400).json('Não foi possível cadastrar o produto!');
         }
 
-        return res.status(200).json('Produto cadastrado com sucesso.');
+        return res.status(200).json('Produto cadastrado com sucesso!');
 
     } catch (error) {
         return res.status(400).json(error.message);
@@ -38,14 +37,14 @@ const listarProdutos = async (req, res) => {
 }
 
 const obterProduto = async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
     try {
 
         const produtos = await conexao.query('SELECT * FROM produtos WHERE id = $1', [id]);
 
         if (produtos.rowCount === 0) {
-            return res.status(404).json('Produto não encontrado');
+            return res.status(404).json('Produto não encontrado!');
         }
 
         return res.status(200).json(produtos.rows[0]);
@@ -55,8 +54,69 @@ const obterProduto = async (req, res) => {
     }
 }
 
+const atualizarProduto = async (req, res) => {
+    const { id } = req.params;
+    const { nome, valor } = req.body;
+
+    try {
+
+        const produtos = await conexao.query('SELECT * FROM produtos WHERE id = $1', [id]);
+
+        if (produtos.rowCount === 0) {
+            return res.status(404).json('Produto não encontrado!');
+        }
+
+        if (!nome) {
+            return res.status(400).json('O campo nome é obrigatório!');
+        }
+
+        if (!valor) {
+            return res.status(400).json('O campo valor é obrigatório!');
+        }
+
+        const query = ('UPDATE produtos SET nome = $1, valor = $2 WHERE id = $3');
+
+        const produtoAtualizado = await conexao.query(query, [nome, valor, id]);
+
+        if (produtoAtualizado.rowCount === 0) {
+            return res.status(404).json('Não foi possível atualizar o produto!');
+        }
+
+        return res.status(200).json('O produto foi atualizado com sucesso!');
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const deletarProduto = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const produto = await conexao.query('SELECT * FROM produtos WHERE id = $1', [id]);
+
+        if (produto.rowCount === 0) {
+            return res.status(404).json('Produto não encontrado!');
+        }
+
+        const query = ('DELETE FROM produtos WHERE id = $1');
+        const produtoDeletado = await conexao.query(query, [id]);
+
+        if (produtoDeletado.rowCount === 0) {
+            return res.status(404).json('Não foi possível excluir o produto!');
+        }
+
+        return res.status(200).json('Produto excluido com sucesso!');
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
 module.exports = {
     criarProduto,
     listarProdutos,
-    obterProduto
+    obterProduto,
+    atualizarProduto,
+    deletarProduto
 }
--- rotas.js
+++ rotas.js
@@ -6,5 +6,7 @@ const rotas = express();
 rotas.post('/produto', produto.criarProduto);
 rotas.get('/produto', produto.listarProdutos);
 rotas.get('/produto/:id', produto.obterProduto);
+rotas.put('/produto/:id', produto.atualizarProduto);
+rotas.delete('/produto/:id', produto.deletarProduto);
 
 module.exports = rotas;
