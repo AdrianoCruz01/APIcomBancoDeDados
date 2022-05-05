f9fefdcc79e235205527329a02ab2e524b239ad4
AdrianoCruz01 <adrianocruz.reis@gmail.com>
2022-05-05 12:52:46 -0300

    Adicionei a rota de produtos

--- rotas.js
+++ rotas.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const produto = require('./controladores/produto');
+const usuarios = require('./controladores/usuarios')
 
 const rotas = express();
 
@@ -8,5 +9,8 @@ rotas.get('/produto', produto.listarProdutos);
 rotas.get('/produto/:id', produto.obterProduto);
 rotas.put('/produto/:id', produto.atualizarProduto);
 rotas.delete('/produto/:id', produto.deletarProduto);
+
+rotas.post('/cadastrar', usuarios.cadastrarUsuario);
+rotas.post('/login', usuarios.login);
 
 module.exports = rotas;
